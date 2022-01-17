57c5734451e041221457eede6668480c66a11dce
InsanRycbekov77 <insanrycbecov@gmail.com>
2022-01-17 15:39:07 +0600

    master

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>Insan</div>
+      <div>
+        <ul>
+          <li>I love you</li>
+          <li>jed</li>
+        </ul>
+      </div>
     </div>
   );
 }
