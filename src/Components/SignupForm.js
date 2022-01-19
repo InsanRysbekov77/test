3cee779c7e3881e9b4f73778e88edc6d64ffd767
InsanRycbekov77 <insanrycbecov@gmail.com>
2022-01-19 18:28:14 +0600

    dfg

--- src/Components/SignupForm.js
+++ src/Components/SignupForm.js
@@ -1,13 +1,10 @@
-import { useEffect, useState } from "react"
-import Validation from "./Validation"
+import { useEffect, useState } from 'react'
+import Validation from './Validation'
 
-
-
-const SignupForm = ({ submitForm} ) => {
-
+const SignupForm = ({ submitForm }) => {
 	const [fullname, setFullname] = useState('')
 	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('') 
+	const [password, setPassword] = useState('')
 
 	const [errors, setErrors] = useState({})
 	const [dataIsCorrect, setDataIsCorrect] = useState(false)
@@ -30,17 +27,16 @@ const SignupForm = ({ submitForm} ) => {
 		setErrors(Validation(email))
 		setErrors(Validation(password))
 		setDataIsCorrect(true)
-		// console.log(fullname);
-		// console.log(email);
-		// console.log(password);
-
+		console.log(fullname)
+		console.log(email)
+		console.log(password)
 	}
 
-	useEffect(()=>{
-		if(Object.keys(errors).length === 0 && dataIsCorrect){
+	useEffect(() => {
+		if (Object.keys(errors).length === 0 && dataIsCorrect) {
 			submitForm(true)
 		}
-	},[errors])
+	})
 	return (
 		<div className='container'>
 			<div className='app-wrapper'>
@@ -50,32 +46,38 @@ const SignupForm = ({ submitForm} ) => {
 				<form onSubmit={submitHadler} className='form-wrapper'>
 					<div className='name'>
 						<label className='label'>Full Name</label>
-						<input 
-						className='input'
-						type='text' 
-						onChange={fullnameChangeHandler} 
+						<input
+							className='input'
+							type='text'
+							onChange={fullnameChangeHandler}
 						/>
-						{errors.fullname && <p className="error">{errors.fullname}</p>}
+						{errors.fullname && (
+							<p className='error'>{errors.fullname}</p>
+						)}
 					</div>
 					<div className='email'>
 						<label className='label'>email</label>
-						<input 
-						 className='input'
-						 type='email' 
-						 onChange={emailChangeHandler}
-						 />
-						 {errors.email && <p className="error">{errors.email}</p>}
+						<input
+							className='input'
+							type='email'
+							onChange={emailChangeHandler}
+						/>
+						{errors.email && (
+							<p className='error'>{errors.email}</p>
+						)}
 					</div>
-					<div className='name'>
+					<div className='password'>
 						<label className='label'>password</label>
-						<input 
-						 className='input' 
-						 type='password' 
-						 onChange={passwordChangeHandler} 
-						 />
-						 {errors.password && <p className="error">{errors.password}</p>}
+						<input
+							className='input'
+							type='password'
+							onChange={passwordChangeHandler}
+						/>
+						{errors.password && (
+							<p className='error'>{errors.password}</p>
+						)}
 					</div>
-					<button className='submit' >Sing Up</button>
+					<button className='submit'>Sing Up</button>
 				</form>
 			</div>
 		</div>
