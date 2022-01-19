b6509fa79c8e74f8ebe39151b85b2ec7280dc6c3
InsanRycbekov77 <insanrycbecov@gmail.com>
2022-01-19 14:18:32 +0600

    Objectdone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
+import Form from './Components/Form';
+
+
 
 function App() {
   return (
     <div className="App">
-      <div>Insan</div>
+      <Form />
     </div>
   );
 }
