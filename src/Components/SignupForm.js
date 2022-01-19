43c5ed0841c20693cfb2a2014948076eb8ec1533
InsanRycbekov77 <insanrycbecov@gmail.com>
2022-01-19 15:40:38 +0600

    super

--- src/Components/SignupForm.js
+++ src/Components/SignupForm.js
@@ -1,5 +1,31 @@
-import React from "react"
+import { useState } from "react"
+
+
+
 const SignupForm = () => {
+
+	const [fullname, setFullname] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('') 
+
+	const fullnameChangeHandler = (event) => {
+		setFullname(event.target.value)
+	}
+
+	const emailChangeHandler = (event) => {
+		setEmail(event.target.value)
+	}
+
+	const passwordChangeHandler = (event) => {
+		setPassword(event.target.value)
+	}
+
+	const submitHadler = (event) => {
+		event.preventDefault()
+		console.log(fullname);
+		console.log(email);
+		console.log(password);
+	}
 
 	return (
 		<div className='container'>
@@ -7,20 +33,20 @@ const SignupForm = () => {
 				<div>
 					<h2 className='title'>Create Accaunt</h2>
 				</div>
-				<form className='form-wrapper'>
+				<form onSubmit={submitHadler} className='form-wrapper'>
 					<div className='name'>
 						<label className='label'>Full Name</label>
-						<input className='input' type='text'></input>
+						<input className='input' type='text' onChange={fullnameChangeHandler}></input>
 					</div>
 					<div className='email'>
 						<label className='label'>email</label>
-						<input className='input' type='email'></input>
+						<input className='input' type='email' onChange={emailChangeHandler}></input>
 					</div>
 					<div className='name'>
 						<label className='label'>password</label>
-						<input className='input' type='password'></input>
+						<input className='input' type='password' onChange={passwordChangeHandler}></input>
 					</div>
-					<div className='submit'>Sing Up</div>
+					<button className='submit'>Sing Up</button>
 				</form>
 			</div>
 		</div>
